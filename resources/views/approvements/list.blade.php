d257ccc2c0882931feeb88ddfe9605d4ef91bc8f
sanyath007 <sanyath007@gmail.com>
2020-11-23 15:53:20 +0700

    Edit pagination data of approvement list view

--- resources/views/approvements/list.blade.php
+++ resources/views/approvements/list.blade.php
@@ -148,16 +148,20 @@
                     </div><!-- /.box-body -->
 
                     <div class="box-footer clearfix">
+                        <div class="col-md-6" style="font-size: 12px;">
+                            Total @{{ pager.total | currency : "" : 0 }} รายการ
+                        </div>
+                        
                         <ul class="pagination pagination-sm no-margin pull-right">
 
                             <li ng-if="pager.current_page !== 1">
-                                <a ng-click="getDataWithURL(pager.first_page_url)" aria-label="Previous">
+                                <a ng-click="getDataWithURL(pager.path + '?page=1')" aria-label="Previous">
                                     <span aria-hidden="true">First</span>
                                 </a>
                             </li>
                         
                             <li ng-class="{'disabled': (pager.current_page==1)}">
-                                <a ng-click="getDataWithURL(pager.first_page_url)" aria-label="Prev">
+                                <a ng-click="getDataWithURL(pager.prev_page_url)" aria-label="Prev">
                                     <span aria-hidden="true">Prev</span>
                                 </a>
                             </li>
@@ -175,7 +179,7 @@
                             </li>
 
                             <li ng-if="pager.current_page !== pager.last_page">
-                                <a ng-click="getDataWithURL(pager.last_page_url)" aria-label="Previous">
+                                <a ng-click="getDataWithURL(pager.path+ '?page=' +pager.last_page)" aria-label="Previous">
                                     <span aria-hidden="true">Last</span>
                                 </a>
                             </li>
