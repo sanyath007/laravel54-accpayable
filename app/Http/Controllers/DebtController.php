0d07932fb289778c4b637dc36a5f3cfacba15da9
sanyath007 <sanyath007@gmail.com>
2022-11-01 15:12:31 +0700

    Add try/catch statment to  store method of DebtController for error handling

--- app/Http/Controllers/DebtController.php
+++ app/Http/Controllers/DebtController.php
@@ -189,44 +189,52 @@ class DebtController extends Controller
 
     public function store(Request $req)
     {
-        /** 0=รอดำเนินการ,1=ขออนุมัติ,2=ตัดจ่าย,3=ยกเลิก,4=ลดหนี้ศุนย์ */
-        $debt = new Debt();
-        $debt->debt_id = $this->generateAutoId();
-        $debt->debt_date = $req['debt_date'];
-        $debt->debt_doc_recno = $req['debt_doc_recno'];
-        $debt->debt_doc_recdate = $req['debt_doc_recdate'];
-        $debt->deliver_no = $req['deliver_no'];
-        $debt->deliver_date = $req['deliver_date'];
-        $debt->debt_doc_no = $req['debt_doc_no'];
-        $debt->debt_doc_date = $req['debt_doc_date'];
-        $debt->debt_type_id = $req['debt_type_id'];
-        $debt->debt_type_detail = $req['debt_type_detail'];
-        $debt->debt_month = $req['debt_month'];
-        $debt->debt_year = $req['debt_year'];
-        $debt->supplier_id = $req['supplier_id'];
-        $debt->supplier_name = $req['supplier_name'];
-        $debt->doc_receive = $req['doc_receive'];
-        $debt->debt_amount = $req['debt_amount'];
-        $debt->debt_vatrate = $req['debt_vatrate'];
-        $debt->debt_vat = $req['debt_vat'];
-        $debt->debt_total = $req['debt_total'];
-        $debt->debt_remark = $req['debt_remark'];
-        
-        $debt->debt_creby = $req['debt_creby'];
-        $debt->debt_credate = date("Y-m-d H:i:s");
-        $debt->debt_userid = $req['debt_userid'];
-        $debt->debt_chgdate = date("Y-m-d H:i:s");
-        $debt->debt_status = '0';
+        try{
+            /** 0=รอดำเนินการ,1=ขออนุมัติ,2=ตัดจ่าย,3=ยกเลิก,4=ลดหนี้ศุนย์ */
+            $debt = new Debt();
+            $debt->debt_id = $this->generateAutoId();
+            $debt->debt_date = $req['debt_date'];
+            $debt->debt_doc_recno = $req['debt_doc_recno'];
+            $debt->debt_doc_recdate = $req['debt_doc_recdate'];
+            $debt->deliver_no = $req['deliver_no'];
+            $debt->deliver_date = $req['deliver_date'];
+            $debt->debt_doc_no = $req['debt_doc_no'];
+            $debt->debt_doc_date = $req['debt_doc_date'];
+            $debt->debt_type_id = $req['debt_type_id'];
+            $debt->debt_type_detail = $req['debt_type_detail'];
+            $debt->debt_month = $req['debt_month'];
+            $debt->debt_year = $req['debt_year'];
+            $debt->supplier_id = $req['supplier_id'];
+            $debt->supplier_name = $req['supplier_name'];
+            $debt->doc_receive = $req['doc_receive'];
+            $debt->debt_amount = $req['debt_amount'];
+            $debt->debt_vatrate = $req['debt_vatrate'];
+            $debt->debt_vat = $req['debt_vat'];
+            $debt->debt_total = $req['debt_total'];
+            $debt->debt_remark = $req['debt_remark'];
+            
+            $debt->debt_creby = $req['debt_creby'];
+            $debt->debt_credate = date("Y-m-d H:i:s");
+            $debt->debt_userid = $req['debt_userid'];
+            $debt->debt_chgdate = date("Y-m-d H:i:s");
+            $debt->debt_status = '0';
 
-        if($debt->save()) {
+            if($debt->save()) {
+                return [
+                    "status"    => 1,
+                    "message"   => "Insert success.",
+                    "debt"      => $debt
+                ];
+            } else {
+                return [
+                    "status"    => 0,
+                    "message"   => "Insert failed.",
+                ];
+            }
+        } catch (\Exception $ex) {
             return [
-                "status" => "success",
-                "message" => "Insert success.",
-            ];
-        } else {
-            return [
-                "status" => "error",
-                "message" => "Insert failed.",
+                'status'    => 0,
+                'message'   => $ex->getMessage()
             ];
         }
     }
